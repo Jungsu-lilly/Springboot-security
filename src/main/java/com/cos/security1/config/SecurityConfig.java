a8c237a125166af2ae81277002f43738e6e32c6c
matt <matt1235@naver.com>
2023-01-02 14:15:02 +0900

    feat : Config 추가. 스프링 시큐리티 필터를 스프링 필터체인에 등록 SecurityConfig > 해당 클래스가 스프링 필터체인에 등록된다. 개발자가 어느 요청을 인가(authorization) 해줄지 직접 설정할 수 있다.

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프릥 시큐리티 필터가 스프링 필터체인에 등록된다.
+public class SecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated() // 인증이 필요
+                .antMatchers("/manager/**").access("hasAnyRole('ROLE_MANAGER', 'ROLE_ADMIN')")
+                // 위 권한이 있는 사람만 들어가게 할 것임!
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login");  // 권한이 없는 접근은 모두 login 페이지로
+
+        return http.build();
+    }
+}
